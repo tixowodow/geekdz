c0891af2f8edd478a12f16966d15977ae3d65b2f
Илья Тиховодов <ilatihovodov@Ilas-MacBook-Air.local>
2024-02-17 17:26:15 +0200

    // Создание массива строк

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+        // Ввод размера массива
+        Console.Write("Введите размер массива: ");
+        int size = int.Parse(Console.ReadLine());
+
+        // Создание массива строк
+        string[] originalArray = new string[size];
+        Console.WriteLine("Введите элементы массива:");
+
+        
