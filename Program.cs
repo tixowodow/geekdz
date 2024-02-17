96c424a88347be4a8159445a0dabbd84d60c26fc
Илья Тиховодов <ilatihovodov@Ilas-MacBook-Air.local>
2024-02-17 17:29:36 +0200

    Заполнение массива строк

--- Program.cs
+++ Program.cs
@@ -6,5 +6,12 @@
         // Создание массива строк
         string[] originalArray = new string[size];
         Console.WriteLine("Введите элементы массива:");
+// Заполнение массива строк
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"Элемент {i + 1}: ");
+            originalArray[i] = Console.ReadLine();
+        }
 
+    
         
