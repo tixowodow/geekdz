40e2cb44c21b3f320ff6ca20ce8af2f6ce556cde
Илья Тиховодов <ilatihovodov@Ilas-MacBook-Air.local>
2024-02-17 17:37:10 +0200

    sohranil izmeneniy

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿
+
+   
+    {
         // Ввод размера массива
         Console.Write("Введите размер массива: ");
         int size = int.Parse(Console.ReadLine());
@@ -6,13 +9,15 @@
         // Создание массива строк
         string[] originalArray = new string[size];
         Console.WriteLine("Введите элементы массива:");
-// Заполнение массива строк
+
+        // Заполнение массива строк
         for (int i = 0; i < size; i++)
         {
             Console.Write($"Элемент {i + 1}: ");
             originalArray[i] = Console.ReadLine();
         }
- // Фильтрация массива
+
+        // Фильтрация массива
         string[] filteredArray = FilterStrings(originalArray);
 
         // Вывод результата
@@ -21,7 +26,36 @@
         {
             Console.WriteLine(str);
         }
-    
+    }
 
-    
+    // Метод для фильтрации строк в массиве
+    static string[] FilterStrings(string[] array)
+    {
+        int count = 0;
+
+        // Определение количества строк, удовлетворяющих условию
+        foreach (string str in array)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        // Создание нового массива
+        string[] resultArray = new string[count];
+        int index = 0;
+
+        // Заполнение нового массива
+        foreach (string str in array)
+        {
+            if (str.Length <= 3)
+            {
+                resultArray[index++] = str;
+            }
+        }
+
+        return resultArray;
+    }
+
         
